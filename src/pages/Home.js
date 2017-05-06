46adfbf923a40de18784adea9251b231421e7fd1
nesreenM <nesreen.mouti@gmail.com>
2017-05-06 17:12:36 +0200

    Folder-structered

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router";
 import axios from 'axios';
+import SingleArtist from './SingleArtist.js';
 
 // import Footer from "../components/layout/Footer";
 // import Nav from "../components/layout/Nav";
@@ -12,33 +13,45 @@ let params = {
 	type: 'artist',
 	maxResults: 20
 };
-
-
-
 export default class Home extends React.Component {
   	constructor(props) {
 		super(props);
+		// this.load = this.load.bind(this)
 		this.state = {
-			artists: [],
+			artists: [{}],
+			items: [],
+			name: "",
+			url :""
 		}
-
+		  		console.log("CONSTRUCTOR");
 	}
   	
   	componentWillMount(){
+  		console.log("hommeeeeee");
+
 		let keyword = "lil wayne";
 		params.q = keyword;
-
 		axios.get(API_URL, {params: params}).then(response => {
-			this.setState({artists: response.data.items});
-			console.log(response.data);
+
+			this.setState({artists: response.data.artists});
+			this.setState({items: response.data.artists.items});
+			this.setState({name: response.data.artists.items[0].name});
+			this.setState({url: response.data.artists.items[0].images[2].url});
+			console.log("@@@@@@@@@@@@@@");
+			console.log("response")
+			console.log(this.state.artists);
+			// console.log(this.state.artists.items[0].name);
+			// console.log("###" + response.data.artists.items[0].name);
+			console.log("@@@@@@@@@@@@@@");
 		});
 	}
 
   	render() {
+  		console.log("home render");
   		return (
             <div>
               <h1>Top Artists</h1>
-
+              <SingleArtist art= {this.state.artists} items={this.state.items} name={this.state.name} url={this.state.url}></SingleArtist>
             </div>
 
         );
--- src/pages/SingleArtist.js
+++ src/pages/SingleArtist.js
@@ -1,25 +1,38 @@
 
 import React from "react";
 import { Link } from "react-router";
+import  "../index.css";
 
 // import Footer from "../components/layout/Footer";
 // import Nav from "../components/layout/Nav";
-export default class Layout extends React.Component {
+export default class SingleArtist extends React.Component {
+ constructor(props){
+      super(props);
 
+    }
   render() {
+    if(this.props.art != null){
+      var artists = this.props.art;
+      var name = this.props.name;
+      var url = this.props.url;
+      var items = this.props.items;
+      console.log("==================================");
+      console.log(name);
+      console.log(url);
+    }
+   
+    
     return (
-            <div className="top-artists-area" >
-              	{this.state.artists.map(
-									(artist, i) => <li className="artist-box" key={i} onClick={()=>props.clickerListener(video)}>
-													<img src={video.snippet.thumbnails.default.url} className="artist-image"></img>
-													<div className="artist-name"> {artist.snippet.title}</div>
-												</li>
-								)
-				}
-              </div>
-
-
-
-    );
+            <div >  	
+            <h2>Artist</h2>
+            <ul>
+                <li> 
+                  <img src= {url}/> 
+                 <h6 className= "artist-image">{name}</h6>
+                </li>
+            </ul>
+            </div>
+            );
   }
 }
+
